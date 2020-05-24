9b859dc18f3fe6b57243086e5158ce0c41534222
GC <tanggc_ah@qq.com>
2020-05-24 22:30:31 +0800

    Merge remote-tracking branch 'remotes/IDEA_TEST/master' into dev01
    # Conflicts:
    #	src/test/org/tanggc/VersionControl.java

--- src/test/org/tanggc/Status.java
+++ src/test/org/tanggc/Status.java
@@ -38,6 +38,9 @@ public enum Status {
         System.out.println("tangfeng");
         System.out.println("tangfeng");
         System.out.println("tangfeng");
+        System.out.println("tangfeng");
+        System.out.println("tangfeng");
+        System.out.println("tangfeng");
 
     }
 
